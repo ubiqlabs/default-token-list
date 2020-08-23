a6365d81aa72818558cfd4f69ba0ef17d84f8932
Luke Williams <luke@iquidus.io>
2020-08-23 14:50:49 +0200

    add ubiq

--- src/buildList.js
+++ src/buildList.js
@@ -1,14 +1,11 @@
 const { version } = require('../package.json');
-const mainnet = require('./tokens/mainnet.json');
-const ropsten = require('./tokens/ropsten.json');
-const rinkeby = require('./tokens/rinkeby.json');
-const goerli = require('./tokens/goerli.json');
-const kovan = require('./tokens/kovan.json');
+// const ethereum = require('./tokens/ethereum.json');
+const ubiq = require('./tokens/ubiq.json');
 
 module.exports = function buildList() {
   const parsed = version.split('.');
   return {
-    'name': 'Uniswap Default List',
+    'name': 'Default List',
     'timestamp': (new Date().toISOString()),
     'version': {
       'major': +parsed[ 0 ],
@@ -16,17 +13,13 @@ module.exports = function buildList() {
       'patch': +parsed[ 2 ]
     },
     'tags': {},
-    'logoURI': 'ipfs://QmZgpZq1HkVS7YCThNJeR51KFVo5AL8P3ThA2y9wcw53p9',
     'keywords': [
-      'uniswap',
+      'ubiq',
       'default'
     ],
     tokens: [
-      ...mainnet,
-      ...ropsten,
-      ...goerli,
-      ...kovan,
-      ...rinkeby
+      // ...ethereum,
+      ...ubiq
     ]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
